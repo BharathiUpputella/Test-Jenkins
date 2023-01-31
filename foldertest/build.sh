a80bdade55e9ea8a1e0f88813db478db03412ef9
BharathiUpputella <65863111+BharathiUpputella@users.noreply.github.com>
2023-01-31 12:08:51 +0530

    Update build.sh

--- foldertest/build.sh
+++ foldertest/build.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 #sh buildfe.sh -e stg/prod
-while getopts e: flag
-do
-        case "${flag}" in
-                e) environment=${OPTARG};;
-        esac
-done
-if [[ -z "$environment" ]]
-then
-        echo "Missing Environment arguement : -e stg"
-        exit 1
-fi
 echo "$(yarn install)"
