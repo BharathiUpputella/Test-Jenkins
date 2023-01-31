539c8f11038802af43590d127020f17e95443d34
BharathiUpputella <65863111+BharathiUpputella@users.noreply.github.com>
2023-01-31 12:15:08 +0530

    Update build.sh

--- foldertest/build.sh
+++ foldertest/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 #sh buildfe.sh -e stg/prod
+echo "$(dnf install nodejs)"
+echo "$(node -v)"
+echo "$(npm -v)"
 echo "$(npm install -g yarn)"
 echo "$(yarn --version)"
 echo "$(yarn install)"
